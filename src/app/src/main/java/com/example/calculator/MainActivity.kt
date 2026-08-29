dcc8e7e9d57e135d12ca0d1a7899c2acd8220ad5
ryan <ryan1210.chiu@gmail.com>
2026-08-29 11:09:12 +0800

    Add clear button

--- src/app/src/main/java/com/example/calculator/MainActivity.kt
+++ src/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,14 +2,21 @@ package com.example.calculator
 
 import android.icu.util.Output
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MultiChoiceSegmentedButtonRow
 import androidx.compose.material3.Scaffold
@@ -26,7 +33,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,14 +61,16 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting() {
-    var currentvalue by remember{ mutableStateOf(0)}
-    var display by remember { mutableStateOf("") }
+    var currentstring by remember{ mutableStateOf("")}//目前數值but字串
+    var currentvalue by remember{ mutableStateOf(0)}//目前數值
+    var display by remember { mutableStateOf("") }//顯示
+
     Scaffold(modifier=Modifier
         .fillMaxSize(),
         containerColor = Color.Black,
         topBar = {
             TopAppBar(
-                title = { Text("Calculator", fontSize = 30.sp) },
+                title = { Text("Calculator", fontSize = 40.sp) },
                 colors=TopAppBarDefaults.topAppBarColors(
                     containerColor = Color.Black,
                     titleContentColor = Color.White
@@ -81,10 +93,37 @@ fun Greeting() {
                     unfocusedIndicatorColor = Color.Transparent
                 ),
                 value=display,
-                textStyle = TextStyle(fontSize = 20.sp, color = Color.White),
+                textStyle = TextStyle(fontSize = 30.sp, color = Color.White),
                 onValueChange = {},
                 readOnly = true,
             )
+            Spacer(modifier=Modifier.height(20.dp))
+            //第一排按鈕
+            Row(
+                modifier=Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+            ){
+                //清除按鈕
+                Button(
+                    onClick = {
+                        display=""
+                        currentvalue=0
+                    },
+                    modifier=Modifier
+                        .padding(5.dp)
+                        .weight(1f),
+                    shape = CircleShape,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.DarkGray,
+                        contentColor = Color.Red
+                    )
+                ) {
+                    Text("C", fontSize = 30.sp)
+                }
+
+                
+            }
         }
     }
 }
