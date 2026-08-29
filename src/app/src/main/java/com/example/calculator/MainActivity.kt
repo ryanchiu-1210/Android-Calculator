7db9bea3a4e5fb7c01938b600fc4997a926cd9a4
ryan <ryan1210.chiu@gmail.com>
2026-08-29 10:29:36 +0800

    Set background color and add textfield , title

--- src/app/src/main/java/com/example/calculator/MainActivity.kt
+++ src/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,21 +5,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MultiChoiceSegmentedButtonRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material.Colors
 import com.example.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,31 +46,50 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting() {
-
-    Scaffold(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(10.dp)
-    ){innerPadding->
-        var output by remember{ mutableStateOf("")}
-        var value by remember { mutableStateOf(0) } }
-    TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-                .padding(innerPaddding),
-            value = {
-                Text(
-                    text =
+    var currentvalue by remember{ mutableStateOf(0)}
+    var display by remember { mutableStateOf("") }
+    Scaffold(modifier=Modifier
+        .fillMaxSize(),
+        containerColor = Color.Black,
+        topBar = {
+            TopAppBar(
+                title = { Text("Calculator", fontSize = 30.sp) },
+                colors=TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Black,
+                    titleContentColor = Color.White
                 )
-            }
-        )
+            )
+        }
+    ){innerPadding->
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(innerPadding)
+            .padding(10.dp)
+        ){
+            TextField(modifier=Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Black,
+                    unfocusedContainerColor = Color.Black,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                value=display,
+                textStyle = TextStyle(fontSize = 20.sp, color = Color.White),
+                onValueChange = {},
+                readOnly = true,
+            )
+        }
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
